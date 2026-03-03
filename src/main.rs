1a6c3d76ccead8de8287fbbd09e18980676954f0
Nnaemeka Nwankwo <codebasetwo@gmail.com>
2026-03-04 00:32:55 +0100

    search

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    // Unwrap or unwrap_or_else is used when function returns.
+    // unwrap return panics with a default message
+    // using .expect let us choose the panic mesage instead of default. so
+    // Result you unwrap the value.
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
@@ -12,7 +17,10 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(&Config);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -31,13 +39,17 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        OK(Config { query, file_path })
+        Ok(Config { query, file_path })
     }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // using '?' tells rust to return the error if theres one
+    // also used when a Result is returned.
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents){
+        println!("{line}");
+    }
+    Ok(())
 }
