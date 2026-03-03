ff0a8ba58c355201b61e65510508a4d9227eba9f
Nnaemeka Nwankwo <codebasetwo@gmail.com>
2026-03-03 13:18:59 +0100

    refactor function logic

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    // Read content of the file
-    let contents = fs::read_to_string(config.file_path)
-        .expect("should have read in a file.");
-
-    println!("With text:\n{contents}");
+    run(&Config);
 }
 
 struct Config {
@@ -22,10 +21,23 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            // Instead of panicking we can retun result
+            // panic("Not enough arguments.");
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        OK(Config { query, file_path })
     }
+}
+
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
