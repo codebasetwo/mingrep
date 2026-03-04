a9d2b9cc462830d7237dcb4cc84510ffea18f350
Nnaemeka Nwankwo <codebasetwo@gmail.com>
2026-03-05 00:19:38 +0100

    improve with iterators

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,11 @@ use std::error::Error;
 use minigrep::{search, search_case_insensitive};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
     // Unwrap or unwrap_or_else is used when function returns.
     // unwrap return panics with a default message
     // using .expect let us choose the panic mesage instead of default. so
     // Result you unwrap the value.
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -29,21 +28,27 @@ struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            // Instead of panicking we can retun result
-            // panic("Not enough arguments.");
-            return Err("not enough arguments");
-        }
+    fn build(mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
-        // We don’t care about the value of the environment variable,
-        // just whether it’s set or unset, 'is_ok()'
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path, ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
